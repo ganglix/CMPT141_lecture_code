197365cb53bf1c7035effd10658cdf3ea6a2cc76
Gang <ganglix@gmail.com>
2023-01-09 22:34:53 -0600

    prep from mac

---
+++ week_1/ch01_ex3.py
@@ -0,0 +1,22 @@
+# bonus example: integer division
+# header
+# actions
+
+"""
+Algorithm: integer division
+input: dividend, divisor
+output: integer quotient = dividend/ divisor
+
+remainder = dividend - divisor
+check if remainder >0.
+if so, keep subtracting divisor from  remainder,
+    repeat the subtraction until remainder <0
+count how many of divisors have been subtracted
+quotient = number of subtracted divisors -1
+
+"""
+
+
+
+
+
