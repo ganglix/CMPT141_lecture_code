8d76e6082101df284fdf9b2ac36a2fbcb3428010
Gang <ganglix@gmail.com>
2023-01-17 11:14:12 -0600

    update in class

--- week_2/ch04_ex1_ex2.py
+++ week_2/ch04_ex1_ex2.py
@@ -6,6 +6,7 @@ say_hello()
 
 # (b) Function named copycat() that takes a phrase
 # parameter and prints its value to the screen five times (hint newbie,*)
+
 def copycat(phrase):
     phrase_new = phrase + " "
     print(phrase_new * 5)
@@ -19,3 +20,8 @@ def high_score(p1, p2, p3):
     return highest_value
 
 print( high_score(2, 1, 3) )
+
+
+
+# (c) Function named high_score() that takes three integer
+# parameters and returns the highest value
