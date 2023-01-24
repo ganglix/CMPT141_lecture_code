45a7cecb1b018236e0a550df27fa9faf7c491662
Gang <ganglix@gmail.com>
2023-01-24 10:30:53 -0600

    update in class Jan 24

--- week_3/ch06_demo2.py
+++ week_3/ch06_demo2.py
@@ -4,35 +4,35 @@
 import turtle as turtle
 import random as random
 
-turtle.speed(1)
-turtle.color(0, 0, 1)
-turtle.forward(50) # 50 pixels
-# turtle.color(1, 0, 0)
-# turtle.right(90)
-# turtle.forward(100)
-turtle.done()
-
-# turtle.shape("turtle")
 # turtle.speed(1)
-# turtle.up()
-#
-# def drawCircle(x, y):
-#     # Draw a circle there , and fill it with a random color .
-#     turtle.fillcolor(random.random(), random.random(), random.random())
-#     turtle.goto(x, y)
-#     turtle.down()
-#     turtle.begin_fill()
-#     turtle.circle(30)
-#     turtle.end_fill()
-#     turtle.up()
-#
-# turtle.title ("Click on the canvas !")
-#
-# # bind left mouse button to drawCircle function
-# turtle.onscreenclick(drawCircle)
+# turtle.color(0, 0, 1)
+# turtle.forward(50) # 50 pixels
+# # turtle.color(1, 0, 0)
+# # turtle.right(90)
+# # turtle.forward(100)
+# turtle.done()
+
+turtle.shape("turtle")
+turtle.speed(10)
+turtle.up()
+
+def drawCircle(x, y):
+    # Draw a circle there , and fill it with a random color .
+    turtle.fillcolor(random.random(), random.random(), random.random())
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.circle(30)
+    turtle.end_fill()
+    turtle.up()
+
+turtle.title ("Click on the canvas !")
+
+# bind left mouse button to drawCircle function
+turtle.onscreenclick(drawCircle)
 
 # Initiate main loop
-# turtle.mainloop()
+turtle.mainloop()
 
 
 # spaceship game
--- week_3/ch06_ex1.py
+++ week_3/ch06_ex1.py
@@ -1,2 +1,5 @@
 import math as m
 
+a = 10
+print(m.pow(a, 2))
+
