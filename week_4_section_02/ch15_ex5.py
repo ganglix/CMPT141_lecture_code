57a0791abd2c94d72555a9523a4f6fcf2397ce46
Gang <ganglix@gmail.com>
2023-02-03 13:41:12 -0600

    update in class

--- week_4_section_02/ch15_ex5.py
+++ week_4_section_02/ch15_ex5.py
@@ -6,21 +6,32 @@ def cost_phone_call(call_duration):
     """
 
     # base cost of call
-    cost = 0.35
+    cost = 0.35;  print(f"line 9: cost: {cost}, call_duration: {call_duration}")
 
     # compute cost of call in first ten minutes
-    cost = cost + min(call_duration, 10.0) * 0.50
-    remaining_duration = max(call_duration - 10.0, 0.0)
+    cost = cost + min(call_duration, 10.0) * 0.50;  print(f"line 12: cost: {cost}, call_duration: {call_duration}")
+    remaining_duration = max(call_duration - 10.0, 0.0);  print(f"line 13: cost: {cost}, call_duration: {call_duration}")
 
     # compute cost of call after first ten minutes
     # (only counts full minutes)
-    cost = cost + remaining_duration / 1 * 0.25
+    # import math
+    cost = cost + remaining_duration // 1 * 0.25 ;  print(f"line 18: cost: {cost}, call_duration: {call_duration}")
 
     # discount from plan deducts 10% from total cost
-    cost = cost * 0.9
+    cost = cost * 0.9;  print(f"line 21: cost: {cost}, call_duration: {call_duration}")
+
     return cost
 
 
 # should be 5.04
 bill = cost_phone_call(11.15)
 print(bill)
+
+"""
+# variables local to function
+line#   cost    call_duration   remaining_duration
+9       0.35    11.15
+12      5.35    11.15
+13      5.35    11.15           1.15
+17      5.35+1.15/1*0.25
+"""
--- week_4_section_02/ch15_intro.py
+++ week_4_section_02/ch15_intro.py
@@ -1,7 +1,23 @@
 # why testing
 
-check what the code is supposed to do vs. what the code actually does
+# check what the code is supposed to do vs. what the code actually does
 
 
 
 # proactive - reactive
+
+# pseudo-code
+
+write code
+perform testing
+if fault found:
+    debugging
+    perform testing (updated)
+    if fault found:
+        debugging
+        perfrom another testing
+        ...
+
+all tests passed
+
+
