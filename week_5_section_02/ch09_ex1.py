70c4af9baa3492a54004f3c067b9552546ed4f12
Gang <ganglix@gmail.com>
2023-02-06 12:38:15 -0600

    update in class

--- week_5_section_02/ch09_ex1.py
+++ week_5_section_02/ch09_ex1.py
@@ -3,16 +3,39 @@
 # When this occurs, print the number of attempts made to
 # enter the correct string. e.g. "3 attempt(s) made."
 
-passcode = "cmpt141"
+# passcode = "cmpt141"
 # code = input("input the passcode: ")
-
-
-
-
-
+#
+# # if code == passcode: let's not do this
+# #
+# n_attempts = 1
+# while code != passcode:
+#     code = input("input the passcode: ")
+#     n_attempts += 1
+#
+# print(f"{n_attempts} attempt(s) made.")
+#
+# passcode = "cmpt141"
+# # initial a wrong code
+# code = "absolutely_wrong_!@#$"
+# n_attempts = 0
+# while code != passcode:
+#     code = input("input the passcode: ")
+#     n_attempts += 1
+#
+# print(f"{n_attempts} attempt(s) made.")
 
 
 # Bonous: another option to stop a while loop [not covered in the textbook]
+passcode = "cmpt141"
+n_attempts = 0
+while True:
+    code = input("input the passcode: ")
+    n_attempts += 1
+    if code == passcode:
+        break   # it will stop the loop right here without checking while
+        print("this message will show up the break failed")
 
+print(f"{n_attempts} attempt(s) made.")
 
 
--- week_5_section_02/ch09_intro.py
+++ week_5_section_02/ch09_intro.py
@@ -3,29 +3,63 @@
 # repeat till what? when?
 
 
-# while
+# while - use a condition to make it start and stop
 ## things I want to mention: condition boolean, while/if, keep checking(eg.)
-
 # counting example
-
-
-
+# count = 0
+# while count < 10:
+#     print("count = ", count)
+#     count = count + 1
+#
 # print(f"after the loop is done, count = {count}")
 
+count = 0
+condition = True
+while condition:
+    print("count = ", count)
+    count = count + 1
+    if count == 10:
+        condition = False
+
+print(f"after the loop is done, count = {count}")
+
+# while True: # inf loop
+#     # do something
 
 
 
-# for
-## mention types of sequence
+# for  loop stops when it finishes a sequence
+## mention types of sequence: string, range, list, dict
 
+# for letter in "hello":
+#     print(letter)
 
+# for i in range(10, 50, 3): # 10,13,16...
+#     print(i)
+
+# for i in range(10+1): # 0-10 included
+#     print(i)
 
 # make the Codeflow
 # while loop
-# i = 1
-# while i < 10:
-#     i = i * 2
-# print(i)
+i = 1
+while i < 10:
+    i = i * 2
+print(i)
+"""
+i = 1
+while 1 < 10
+i = 1 * 2
+while 2 < 10
+i = 2 * 2
+while 4 < 10
+i = 4 * 2
+while 8 < 10
+i = 8 * 2
+while 16 < 10
+print(16)
+"""
+
 
 
 # for loop
