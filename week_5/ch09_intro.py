b9bb8102a8cd53c8e5982fac3f51b6b00a56821d
Gang <ganglix@gmail.com>
2023-02-05 23:22:11 -0600

    prep from mac

---
+++ week_5/ch09_intro.py
@@ -0,0 +1,34 @@
+# Control Flow: Repetition
+# what is a computer good at?
+# repeat till what? when?
+
+
+# while
+## things I want to mention: condition boolean, while/if, keep checking(eg.)
+
+# counting example
+
+
+
+# print(f"after the loop is done, count = {count}")
+
+
+
+
+# for
+## mention types of sequence
+
+
+
+# make the Codeflow
+# while loop
+# i = 1
+# while i < 10:
+#     i = i * 2
+# print(i)
+
+
+# for loop
+# for c in "PIKA":
+#     print(c)
+# print("hooray!")
