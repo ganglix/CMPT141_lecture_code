d2b2ea77a3bfed618c4931f6d424127922b7b8fe
Gang <ganglix@gmail.com>
2023-02-14 10:15:40 -0600

    update in class

--- week_6/ch10_ex1.py
+++ week_6/ch10_ex1.py
@@ -11,14 +11,34 @@
 pkg_weights = [2, 6, 8, 34, 56, 67, 4, 2, 33]
 
 # (a) Creates list light_pkgs of parcels that weigh less than 5
-
-
-
+light_pkgs = []
+for p in pkg_weights:
+    if p < 5:
+        light_pkgs.append(p)
+print(light_pkgs)
 
 # (b) Removes parcels from pkg_weights that exceed 15 lbs
+# for p in pkg_weights:   # iterating over a changing list, BIG mistake!!!
+#     if p > 15:
+#         pkg_weights.remove(p)
+# print(pkg_weights)
 
+# # use a clone!!! (advanced!)
+# pkg_weights_copy = pkg_weights[:]  # or use pkg_weights.copy()
+# for p in pkg_weights_copy:
+#     if p > 15:
+#         pkg_weights.remove(p)
+# print(pkg_weights)
 
+# not use a clone
+heavy_pkgs = []
+for p in pkg_weights:
+    if p > 15:
+        heavy_pkgs.append(p)
 
+for p in heavy_pkgs:
+    pkg_weights.remove(p)
+print(pkg_weights)
 
 
 # (c) Print the:
--- week_6/ch10_intro.py
+++ week_6/ch10_intro.py
@@ -1,15 +1,13 @@
 # lists and tuples
 
-
-
 # create a list
-lis = ["a", "b", "c"]
 
+lis = ["a", "b", "c"]  # the element in a list can be anything
 
 # print(type( [] ))
+# print(lis)
+# print(range(3))
 # print(list(range(3)))
-
-
 
 
 # indexing and slicing
@@ -17,32 +15,63 @@ lis = ["a", "b", "c"]
 # "computer"[0],
 #     lis[0:2],
 #     lis[-1],
+#     lis[:100],
+#     sep="\n"
 # )
 
 
-
 # add an item: .append(), +, .extend()
-
-
-
-
-# remove an item: .remove(), del, .pop()
-
-
+# new_thing = lis.append("d")
+# print(f"new_thing: {new_thing}" )
+#
+# lis.append("d")   # in-place change, does not return a new list
+# print(lis)
+# print(lis + ['e', 'f'])
+# # lis.extend('gee')
+# lis.extend(["gee","eich"])
+# print(lis)
+#
+#
+#
+# # remove an item: .remove(), del, .pop()
+# lis.remove('eich')
+# lis.append('a')
+# print(lis)
+# lis.remove('a')
+# print(lis)
+#
+# del lis[0]
+# print(lis)
+#
+# being_removed = lis.pop()  # remove and return the last item
+# print(lis)
+# print(being_removed)
+#
+# being_removed = lis.pop(1)  # remove and return the  item with index 1
+# print(lis)
+# print(being_removed)
 
 
 # # locating an item: index()
-
-
+# c_idx = lis.index('c')
+# print(c_idx)
 
 
 # sorting: .sort()
+# lis = ["c", "a", "b"]
+# lis.sort()
+# print(lis)
+# lis.sort(reverse= True)
+# print(lis)
 
+# lis.append(10)
+# lis.sort()
+# print(lis)
 
+# new_lis = sorted(lis)
+# print(new_lis)
 
-
-
-# list is mutable
+# list is mutable !
 
 
 # function with list
@@ -50,9 +79,24 @@ lis = ["a", "b", "c"]
 
 # codeflow
 # L = [1, 2, 3, 4, 5, 6]
-# for value in L:
+# for value in L:        # do not do it! do not iterate over a changing list
 #     L.remove(value)
 # print(L)
 
 
 # doomed?
+L = [1, 2, 3, 4, 5, 6]
+another_L = L   # this will NOT create a new list
+L.remove(1)
+print(another_L)
+
+L = [1, 2, 3, 4, 5, 6]
+# another_L = L[:]   # this will create a new list clone
+another_L = L.copy()   # this will also create a new list clone
+
+for value in another_L:
+    L.remove(value)
+print(L)
+
+
+tu = ("a", "b", "c")
