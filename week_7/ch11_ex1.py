779fc5ff6d35286ca4e21bb15aec9c3de233aa9c
Gang <ganglix@gmail.com>
2023-02-28 00:20:48 -0600

    prep from mac

---
+++ week_7/ch11_ex1.py
@@ -0,0 +1,18 @@
+# Exercise 1
+
+# Suppose you have a dictionary of survey responses:
+# •keys are student names
+# •values are favourite ice cream flavours (one of
+# "chocolate", "vanilla", "strawberry")
+# Write a function that takes a dictionary of survey responses
+# and returns a new dictionary where:
+# •keys are ice cream flavours
+# •values are number of students with that favourite flavour
+
+survey = {"Christopher": "vanilla",
+          "Ty": "vanilla",
+          "Muhammed": "chocolate",
+          "Ani": "vanilla",
+          "Gang": "chocolate"}
+
+def icecream_survey(survey):
---
+++ week_7/ch11_intro.py
@@ -0,0 +1,22 @@
+# dictionary : hash map in python
+
+# Store and look up data
+
+
+
+
+# KEYS  # use consistent key values
+
+
+# VALUES
+
+# dictionary methods
+
+
+# No key found?
+
+
+
+
+
+# things I want to mention: mutable, no order, key is the key
