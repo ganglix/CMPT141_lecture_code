395905e1ef521b004105adea8e9045b4c06b0161
Gang <ganglix@gmail.com>
2023-03-07 00:09:31 -0600

    prep from mac

---
+++ week_8/ch13_intro.py
@@ -0,0 +1,19 @@
+# beyond basic python
+
+
+# numpy array
+
+
+# create an array
+## 1D array vs list
+
+## 2D array vs list of lists
+
+## nD array vs list of lists of lists...
+
+
+# Arithmetic operation (element-wise) and some useful methods
+
+
+
+# indexing and slicing
